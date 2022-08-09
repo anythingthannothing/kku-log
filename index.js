250f33ff3d6339e62773040370fa2a0871a0d94d
KimKeonU <anythingthannothing@gmail.com>
2022-08-09 21:48:46 +0900

    Add ejs for templating

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 3000;
 
-app.use(() => {
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use((req, res) => {
   console.log("We got a new request!!!");
 });
 
